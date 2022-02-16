03005f9b46b1d5511eefab932ebd8522f3d64da1
Claas Anders Rathje <claasr@thje.net>
2022-02-16 07:05:23 +0100

    Added TFT support

---
+++ README.md
@@ -0,0 +1,21 @@
+# InstaFollowCount
+
+![InstaFollowCount on TTGO T-Display](InstaFollowCount-T-Display.jpg?raw=true)
+
+Use the GraphQL to get the follower count of an Instagram account on an ESP32.
+Uses [ArduinoJson](https://arduinojson.org/) to parse the data, but could be done via string manipulation to save some space.
+Sample with Display for [TTGO T-Display](https://github.com/Xinyuan-LilyGO/TTGO-T-Display).
+
+# Interesting query hashes found on the net:
+
+## Following List
+d04b0a864b4b54837c0d870b0e77e076
+
+## Followers List
+c76146de99bb02f6415203be841dd25a
+
+## Story Viewers List
+42c6ec100f5e57a1fe09be16cd3a7021
+
+## Story List and First Fifty Viewers
+52a36e788a02a3c612742ed5146f1676
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
 #include "WiFi.h"
+#include <Wire.h>
 #include <ArduinoJson.h>
 #include <HTTPClient.h>
+#include <TFT_eSPI.h>
+#include <SPI.h>
 
 const char *ssid = "ONE WIFI TO RULE THEM ALL";
 const char *password = "One Mississippi, Two Mississippi";
@@ -9,12 +12,29 @@ const char *password = "One Mississippi, Two Mississippi";
 WiFiClientSecure secureClient;
 HTTPClient http;
 
+#ifdef ST7789_DRIVER
+TFT_eSPI tft = TFT_eSPI(); // Invoke library, pins defined in User_Setup.h
+#define GFXFF 1
+#endif
+
 long followers = -1;
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println();
+
+// init TFT
+#ifdef ST7789_DRIVER
+  tft.init();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextSize(2);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setCursor(4, 5);
+  tft.println(F("WiFi Connecting"));
+#endif
+
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
   while (WiFi.status() != WL_CONNECTED)
@@ -25,6 +45,9 @@ void setup()
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
+#ifdef ST7789_DRIVER
+  tft.println(WiFi.localIP());
+#endif
 
   // do not validate the chain since the certificates change quite often and I am too lazy for a CA check with time and so on
   secureClient.setInsecure();
@@ -36,8 +59,11 @@ void loop()
   // based on https://stackoverflow.com/questions/32407851/instagram-api-how-can-i-retrieve-the-list-of-people-a-user-is-following-on-ins
   // channel id is: 44601813942
   String followersUrl = "https://www.instagram.com/graphql/query/?query_hash=c76146de99bb02f6415203be841dd25a&variables=%7B%22id%22%3A44601813942%2C%22include_reel%22%3Atrue%2C%22fetch_mutual%22%3Atrue%2C%22first%22%3A50%7D";
-  // hate 302 and 301 to login page
-  followersUrl = "https://iot.ra.thje.net/tmp/twitter-api-follower.json";
+  // hate 302 and 301 to login page due to rate limit for testing locally
+  if (ssid[0] == 'E' && ssid[strlen(ssid) - 1] == 't')
+  {
+    followersUrl = "https://iot.ra.thje.net/tmp/twitter-api-follower.json";
+  }
   http.begin(secureClient, followersUrl);
   ret = http.GET();
   ESP_LOGI(TAG, "GET: %d", ret);
@@ -48,6 +74,21 @@ void loop()
     deserializeJson(followerDoc, http.getStream());
     followers = followerDoc["data"]["user"]["edge_followed_by"]["count"];
     Serial.printf("followers: %ld\n", followers);
+#ifdef ST7789_DRIVER
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextSize(1);
+    tft.setCursor(0, 0);
+    tft.setTextDatum(TC_DATUM); // Centre text on x,y position
+    tft.setFreeFont(&FreeSerifBold12pt7b);
+    tft.setTextColor(TFT_BLUE, TFT_BLACK);
+    tft.drawString("CatInTheDiceBag", tft.width() / 2, 5, GFXFF);
+    tft.setTextColor(TFT_GOLD, TFT_BLACK);
+    tft.drawString("followers", tft.width() / 2, 5 + tft.fontHeight(GFXFF), GFXFF);
+    tft.setTextSize(2);
+    tft.setTextColor(TFT_RED, TFT_BLACK);
+    tft.setFreeFont(&FreeSerifBold18pt7b);
+    tft.drawString(String(followers), tft.width() / 2, tft.height() - tft.fontHeight(GFXFF) + 20, GFXFF);
+#endif
   }
   else
   {
