6e6c6e5dbab1ad5e2054434b2b2c217e24aa0779
Claas Anders Rathje <claasr@thje.net>
2022-02-17 12:18:30 +0100

    Added cached service Optimized redraw a bit

--- src/main.cpp
+++ src/main.cpp
@@ -32,10 +32,13 @@ void setup()
   tft.init();
   tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
-  tft.setTextSize(2);
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.setCursor(4, 5);
-  tft.println(F("WiFi Connecting"));
+  tft.setCursor(0, 5);
+  tft.setTextSize(2);
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.println(F("WiFi Connecting to:"));
+  tft.setTextColor(TFT_LIGHTGREY, TFT_BLACK);
+  tft.println(ssid);
 #endif
 
   WiFi.begin(ssid, password);
@@ -54,6 +57,7 @@ void setup()
 
   // do not validate the chain since the certificates change quite often and I am too lazy for a CA check with time and so on
   secureClient.setInsecure();
+  http.setUserAgent(F("Mozilla/5.0 (Linux; Android 11; LM-G900)"));
 }
 
 int followersByGraphQL(String channelid, long *followers)
@@ -61,11 +65,6 @@ int followersByGraphQL(String channelid, long *followers)
   int ret = -1;
   // based on https://stackoverflow.com/questions/32407851/instagram-api-how-can-i-retrieve-the-list-of-people-a-user-is-following-on-ins
   String followersUrl = "https://www.instagram.com/graphql/query/?query_hash=c76146de99bb02f6415203be841dd25a&variables=%7B%22id%22%3A" + channelid + "%2C%22include_reel%22%3Atrue%2C%22fetch_mutual%22%3Atrue%2C%22first%22%3A50%7D";
-  // hate 302 and 301 to login page due to rate limit for testing locally
-  // if (ssid[0] == 'E' && ssid[strlen(ssid) - 1] == 't')
-  // {
-  //   followersUrl = "https://iot.ra.thje.net/tmp/twitter-api-follower.json";
-  // }
   http.begin(secureClient, followersUrl);
   ret = http.GET();
   ESP_LOGI(TAG, "GET: %d", ret);
@@ -90,9 +89,44 @@ int followersByGraphQL(String channelid, long *followers)
   http.end();
   return -1;
 }
+
+#ifdef USECACHEDSERVICETHATWASNEVERINTENDEDASPUBLIC
+int followersByCachedService(String channelname, long *followers)
+{
+  int ret = -1;
+  // this is some quick 'n dirty way to cache the stuff and always provide a small json generated from
+  // some sites with internal fallback.
+  // easier to adapt later on in case any of the sites change -> change on place and all clients continue working
+  // needs whitelisting for accounts in order to not flood my webspace that much in case this slips out of hand
+  String followersUrl = "https://iot.ra.thje.net/instainfo/?channel=" + channelname;
+  http.begin(secureClient, followersUrl);
+  ret = http.GET();
+  ESP_LOGI(TAG, "GET: %d", ret);
+
+  if (ret == HTTP_CODE_OK)
+  {
+    DynamicJsonDocument followerDoc(2048);
+    deserializeJson(followerDoc, http.getStream());
+    long newfollowers = -1;
+    newfollowers = followerDoc["followers"];
+    if (newfollowers > -1)
+    {
+      *followers = newfollowers;
+    }
+    http.end();
+    return 0; // success
+  }
+  else
+  {
+    ESP_LOGE(TAG, "HTTP Error: %d", ret);
+  }
+  http.end();
+  return -1;
+}
+#endif
+
 int followersByAnonSite(String channelname, long *followers, String siteURL)
 {
-
   int ret = -1;
   String followersUrl = siteURL + channelname;
   http.begin(secureClient, followersUrl);
@@ -131,15 +165,21 @@ int followersByAnonSite(String channelname, long *followers, String siteURL)
 
 int followersByGreatFon(String channelname, long *followers)
 {
-  return followersByAnonSite(channelname, followers, "https://greatfon.com/v/");
+  String lowercasename = channelname;
+  lowercasename.toLowerCase();
+  return followersByAnonSite(lowercasename, followers, "https://greatfon.com/v/");
 }
+
 int followersByDUMPOR(String channelname, long *followers)
 {
-  return followersByAnonSite(channelname, followers, "https://dumpor.com/v/");
+  String lowercasename = channelname;
+  lowercasename.toLowerCase();
+  return followersByAnonSite(lowercasename, followers, "https://dumpor.com/v/");
 }
 
 void loop()
 {
+  long followers_pre = followers;
   bool updateSucceeded = false;
   if (!updateSucceeded)
   {
@@ -174,28 +214,47 @@ void loop()
       updateSucceeded = true;
     }
   }
+#ifdef USECACHEDSERVICETHATWASNEVERINTENDEDASPUBLIC
+  if (!updateSucceeded)
+  {
+    if (followersByCachedService(String(channelname), &followers) != 0)
+    {
+      ESP_LOGE(TAG, "Could not get via Cached Service");
+    }
+    else
+    {
+      updateSucceeded = true;
+    }
+  }
+#endif
   if (followers > -1)
   {
-    Serial.printf("followers: %ld\n", followers);
+    Serial.printf("%10lu:: Followers: %ld\n", millis(), followers);
 #ifdef ST7789_DRIVER
     tft.setTextSize(1);
     tft.setCursor(0, 0);
     tft.setTextDatum(TC_DATUM); // Center text on x,y position
     tft.setFreeFont(&FreeSerifBold12pt7b);
     tft.setTextColor(TFT_BLUE, TFT_BLACK);
-    tft.fillScreen(TFT_BLACK);
-    tft.drawString(channelname, tft.width() / 2, 5, GFXFF);
-    tft.setTextColor(TFT_GOLD, TFT_BLACK);
-    tft.drawString("followers", tft.width() / 2, 5 + tft.fontHeight(GFXFF), GFXFF);
-    tft.setTextSize(2);
-    tft.setTextColor(TFT_RED, TFT_BLACK);
-    tft.setFreeFont(&FreeSerifBold18pt7b);
-    tft.drawString(String(followers), tft.width() / 2, tft.height() - tft.fontHeight(GFXFF) + 20, GFXFF);
+    if (followers_pre == -1) // only clear screen on first successful retrieval, otherwise the black BG of the font is enogh redraw
+    {
+      tft.fillScreen(TFT_BLACK);
+      tft.drawString(channelname, tft.width() / 2, 5, GFXFF);
+      tft.setTextColor(TFT_GOLD, TFT_BLACK);
+      tft.drawString("followers", tft.width() / 2, 5 + tft.fontHeight(GFXFF), GFXFF);
+    }
+    if (followers_pre != followers)
+    {
+      tft.setTextSize(2);
+      tft.setTextColor(TFT_RED, TFT_BLACK);
+      tft.setFreeFont(&FreeSerifBold18pt7b);
+      tft.drawString(String(followers), tft.width() / 2, tft.height() - tft.fontHeight(GFXFF) + 20, GFXFF);
+    }
 #endif
   }
   if (updateSucceeded)
   {
-    sleep(10 * 60);
+    sleep(30 * 60); // every 30min to overcome rate limit
   }
   // esp_sleep_enable_timer_wakeup(60UL * 1000000UL);
   // esp_deep_sleep_start();
